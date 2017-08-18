c6503780bb99a5ab696cffa88d41b6b0d68b35a5
zjy_james <409493471@qq.com>
2017-08-18 15:14:59 +0800

    首次提交到本地仓库

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * piaozhijia.com Inc.
+ * Copyright (c) 2004-2017 All Rights Reserved.
+ */
+package com.test;
+
+/**
+ * 
+ * @author jiuyangzhang2
+ * @version $Id: HelloWorld.java, v 0.1 2017年8月18日 下午3:00:08 jiuyangzhang2 Exp $
+ */
+public class HelloWorld {
+
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+
+}
